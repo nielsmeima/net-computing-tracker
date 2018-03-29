142e9fcabb94eb5a06b87028f266d4f2e60e4a3a
Niels Meima <nielsmeima@Nielss-MacBook-Pro.local>
2018-03-29 13:07:56 +0200

    fix stupid meuk

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+console.log('Init webserver');
+
 const app = express();
 app.use(bodyParser.urlencoded({'extended': 'false' }));
 app.use(bodyParser.json());
@@ -9,10 +11,11 @@ const distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 app.get('/*', function(req, res) {
+  console.log('Serving the HTML')
   res.sendFile(distDir + 'index.html');
 });
 
-const port = 1337;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 

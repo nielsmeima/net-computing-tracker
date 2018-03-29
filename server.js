6258c4095a73fb139292cc7a20cec0c6b55640a8
Niels Meima <nielsmeima@Nielss-MacBook-Pro.local>
2018-03-29 14:04:40 +0200

    working tracker

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const bodyParser = require('body-parser');
 console.log('Init webserver');
 
 const app = express();
-app.use(bodyParser.urlencoded({'extended': 'false' }));
+app.use(bodyParser.urlencoded({'extended': 'true' }));
 app.use(bodyParser.json());
 
 const distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+app.post('/location', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+})
 
 app.get('/*', function(req, res) {
   console.log('Serving the HTML')

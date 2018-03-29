1e62085f4b90df1958eef60d05d33495431cec60
Niels Meima <nielsmeima@Nielss-MacBook-Pro.local>
2018-03-29 16:51:12 +0200

    sae

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const net     = require('net');
+
+// ===== Socket =====
+let client;
+
+try 
+{
+  client = net.createConnection({ port: 8124, host: '192.168.1.19' }, () => {
+    //'connect' listener
+    console.log('connected to server!');
+  });
+
+  // Receiving data from server
+  client.on('data', (data) => {
+    console.log('received confirmation');
+  });
+
+  // Ended connection
+  client.on('end', () => {
+    console.log('disconnected from server');
+  });
+}
+catch (err)
+{
+  console.log(err);
+}
+
+
+const sendLocation = (update) => {
+
+  const buffer = Buffer.from(JSON.stringify(update));
+  console.log('Sjors sent his location');
+  client.write(buffer);
+}
+
+// ===== HTML =====
 
 console.log('Init webserver');
 
@@ -11,8 +47,9 @@ const distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 app.post('/location', (req, res) => {
-  console.log(req.body);
   res.sendStatus(200);
+
+  sendLocation(req.body)
 })
 
 app.get('/*', function(req, res) {
@@ -20,7 +57,5 @@ app.get('/*', function(req, res) {
   res.sendFile(distDir + 'index.html');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1111;
 app.listen(port);
-
-
